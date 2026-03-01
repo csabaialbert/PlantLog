128bd5e9075a29041dcd9f068b0d2809632d74d4
csabaialbert <68478226+csabaialbert@users.noreply.github.com>
2026-03-01 16:34:08 +0100

    IdentityDbMigrations

--- PlantLog/Data/Migrations/20260301153144_InitialCreate.Designer.cs
+++ PlantLog/Data/Migrations/20260301153144_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlantLog.Data;
 
@@ -11,9 +12,11 @@ using PlantLog.Data;
 namespace PlantLog.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260301153144_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
